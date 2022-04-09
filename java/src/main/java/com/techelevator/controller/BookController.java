265e5c8c913db681250e4542cad8d16e04f01142
Naveenajancy Kasi Balasamy <naveenajancykb@gmail.com>
2022-04-09 17:15:48 -0400

    addedd cors for bookcontroller

--- java/src/main/java/com/techelevator/controller/BookController.java
+++ java/src/main/java/com/techelevator/controller/BookController.java
@@ -3,21 +3,22 @@ package com.techelevator.controller;
 import com.techelevator.dao.BookDetailDao;
 import com.techelevator.model.bookmodel.BookDetail;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class BookController {
     @Autowired
     private BookDetailDao bookDetailDao;
 
 
-    @RequestMapping(path="/books", method = RequestMethod.GET)
-    public List<BookDetail> retrieveAllBooks(){
+    @RequestMapping(path = "/books", method = RequestMethod.GET)
+    public List<BookDetail> retrieveAllBooks() {
         return bookDetailDao.getAllBooks();
     }
 }
