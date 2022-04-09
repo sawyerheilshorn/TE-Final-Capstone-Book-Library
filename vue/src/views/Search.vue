e59508caa399d6f1e1febbc3a1bf072f0c6910f5
Michael Lengen <lengen.6@gmail.com>
2022-04-09 15:43:43 -0400

    having issues with web service call

--- vue/src/views/Search.vue
+++ vue/src/views/Search.vue
@@ -1,26 +1,33 @@
 <template>
   <div>
-    <book-card
-      v-bind:book="book"
-      v-for="book in fileredBooks"
-      v-bind:key="book.isbn"
-    />
+    <div v-for="book in books" v-bind:key="book.isbn">
+      <h1>{{ book.title }}</h1>
+      <img
+        v-if="book.isbn"
+        v-bind:src="
+          'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'
+        "
+      />
+      <h3>{{ book.author }}</h3>
+    </div>
   </div>
 </template>
 
 <script>
-import BookCard from "../components/BookCard.vue";
+import BookService from "../services/BookService";
 export default {
+  components: {},
   name: "Search",
-  components: {
-    BookCard,
-  },
   data() {
     return {
       books: [],
     };
   },
-  created() {},
+  created() {
+    BookService.listBooks().then()((response) => {
+      this.books = response.data;
+    });
+  },
 };
 </script>
 
