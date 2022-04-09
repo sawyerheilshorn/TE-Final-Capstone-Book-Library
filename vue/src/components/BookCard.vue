64d7e567e6587d974f31c641ce9bc9c40a88eefa
Michael Lengen <lengen.6@gmail.com>
2022-04-09 14:53:52 -0400

    got bookcard and view and routes setup

---
+++ vue/src/components/BookCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>
+      <h1>{{ book.title }}</h1>
+      <img
+        v-if="book.isbn"
+        v-bind:src="
+          'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'
+        "
+      />
+      <h3>{{ book.author }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "book-card",
+  prop: {
+    book: Object,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue/src/views/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <book-card
+      v-bind:book="book"
+      v-for="book in fileredBooks"
+      v-bind:key="book.isbn"
+    />
+  </div>
+</template>
+
+<script>
+import BookCard from "../components/BookCard.vue";
+export default {
+  name: "Search",
+  components: {
+    BookCard,
+  },
+  data() {
+    return {
+      books: [],
+    };
+  },
+  created() {},
+};
+</script>
+
+<style>
+</style>
