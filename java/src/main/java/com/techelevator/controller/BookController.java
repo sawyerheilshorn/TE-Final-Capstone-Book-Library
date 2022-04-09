bc1ac8aa08d9391d0aeb93458ed138d2a57d8391
Naveenajancy Kasi Balasamy <naveenajancykb@gmail.com>
2022-04-09 10:27:04 -0400

    created data to get all books

--- java/src/main/java/com/techelevator/controller/BookController.java
+++ java/src/main/java/com/techelevator/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,8 +15,35 @@ public class BookController {
     @Autowired
     private BookDetailDao bookDetailDao;
 
+
     @RequestMapping(path="/books", method = RequestMethod.GET)
     public List<BookDetail> retrieveAllBooks(){
-        return bookDetailDao.getAllBooks();
+        List<BookDetail> bookDetailList = new ArrayList<>();
+        BookDetail bookDetail = new BookDetail();
+        bookDetail.setIsbn("9781400079278");
+        bookDetail.setTitle("Kafka by the Shore");
+        bookDetail.setAuthor("Haruki Murakami");
+
+        BookDetail bookDetail1 = new BookDetail();
+        bookDetail1.setIsbn("9780356500157");
+        bookDetail1.setTitle("The Girl With All the Gifts");
+        bookDetail1.setAuthor("M.R. Carey");
+
+        BookDetail bookDetail2 = new BookDetail();
+        bookDetail2.setIsbn("9780684830490");
+        bookDetail2.setTitle("The Old Man and the Sea");
+        bookDetail2.setAuthor("Ernest Hemingway");
+
+        bookDetailList.add(bookDetail);
+        bookDetailList.add(bookDetail1);
+        bookDetailList.add(bookDetail2);
+
+        return bookDetailList;
+
+
+
+
+
+       // return bookDetailDao.getAllBooks();
     }
 }
