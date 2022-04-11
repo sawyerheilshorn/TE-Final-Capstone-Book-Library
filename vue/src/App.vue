82c9418385ab577c45f71a5e6f1d01bcc657b4ad
abdalla mosa <amosa.sij@gmail.com>
2022-04-11 11:57:23 -0400

    push part of nav bar done

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,8 +2,20 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'search' }">Search</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'Login' }">Login</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+
+<style>
+*{
+  margin: 0%;
+  padding: 0%;
+  
+}
+
+
+</style>
