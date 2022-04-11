7f086e571c0e37aa6f975afcdacf1547f832e390
abdalla mosa <amosa.sij@gmail.com>
2022-04-11 14:40:58 -0400

    push navabar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div id="app">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <div id="nav">
+    
+    <div>
       <nav class="navbar sticky-top navbar-expand-lg bg-dark">
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto w-100 justify-content-end">
+          <div class="container">
+          <a class="navbar-brand" href="#">Logo</a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav ">
             <li class="nav-item">
-              <router-link class="nav-link" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+              <router-link class="nav-link" v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;&nbsp;
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" v-bind:to="{ name: 'search' }">Search</router-link>&nbsp;|&nbsp;
+              <router-link class="nav-link" v-bind:to="{ name: 'search' }">Search</router-link>&nbsp;&nbsp;
             </li>  
             <li class="nav-item">
-              <router-link class="nav-link" v-bind:to="{ name: 'Login' }">Login</router-link>&nbsp;|&nbsp;
+              <router-link class="nav-link" v-bind:to="{ name: 'Login' }">Login</router-link>&nbsp;&nbsp;
             </li>
             <li class="nav-item">  
               <router-link class="nav-link" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
             </li>  
           </ul>
         </div>
+          </div>
       </nav>
     </div>
     <router-view />
@@ -35,17 +42,22 @@ body {
   height: 1000px;
 }
 .navbar .navbar-brand {
+  margin-left: 10% ;
   font-size: 30px;
+  color: aliceblue;
 }
 .navbar .nav-item {
   padding: 10px 20px;
+  
 }
+
 .navbar .nav-link {
   font-size: 20px;
   margin-left: 10px;
+  color: aliceblue;
 }
 .fa-bars {
-  color: #007bff;
+  color: #ff1515;
   font-size: 30px;
 }
 
