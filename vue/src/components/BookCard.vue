42c20d9dce7c948e587c793226de2ad39c6becb2
abdalla mosa <amosa.sij@gmail.com>
2022-04-12 17:58:43 -0400

    puah

--- vue/src/components/BookCard.vue
+++ vue/src/components/BookCard.vue
@@ -1,30 +1,20 @@
 <template>
- 
-    <div class="row row-cols-1 row-cols-md-3">
-      <div class="col mb-4">
-      <div class="card text-center">
-        <img
-          v-if="book.isbn"
-          v-bind:src="
-            'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'
-          "
-          class="card-img-top"
-          alt="#"
-        />
-        <div class="card-body">
-          <h5 class="card-title">{{ book.title }}</h5>
-          <h6>{{ "By " + book.author }}</h6>
-          <p
-            id="inline"
-            v-for="genre in book.genreList"
-            v-bind:key="genre.id"
-            class="card-text"
-          >
-            {{ genre.genre_name }}
-          </p>
-        </div>
+  <div>
+   
+    <div id="inline" class="card">
+      <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" class="card-img-top" alt="#" />
+      <div class="card-body">
+        <h5 class="card-title">{{ book.title }}</h5>
+        <h6>{{ "By " + book.author }}</h6>
+        <p id="inline" v-for="genre in book.genreList" v-bind:key="genre.id" class="card-text">
+          {{ genre.genre_name }}
+        </p>
+        
+        
+        
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -36,16 +26,11 @@ export default {
 };
 </script>
 
-<style scoped >
-#inline {
+<style>
+#inline{
   display: inline;
 }
-
-
-
-/*.card-img-top{
-  height: 50%;
-  width: 30%;
-}*/
-
+.card{
+  width: 18rem;
+}
 </style>
